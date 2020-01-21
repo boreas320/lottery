040ca58f8555c18de27421c5b0818970598e2745
Boreas320 <xiangshuai@meituan.com>
2020-01-21 21:14:15 +0800

    添加配置文件

--- js/member.js
+++ js/member.js
@@ -1,6 +1,13 @@
 var member = [];
-for (var i = 1; i <= 450; i++) {
-    var digit = 3;
+var total = config.total;
+var digit = 0;
+var tmpTotal = total;
+while (tmpTotal > 0) {
+    digit++;
+    tmpTotal = parseInt(tmpTotal / 10);
+}
+for (var i = 1; i <= total; i++) {
+
     var mem = {"name": "" + (Array(digit).join(0) + i).slice(-digit)};
     member.push(mem);
 }
