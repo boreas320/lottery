3e768f071e6b59c4f70638ed2690b60251a5e0c3
xiangshuai <xiangshuai@meituan.com>
2020-01-24 01:01:03 +0800

    添加注释说明

--- js/member.js
+++ js/member.js
@@ -2,7 +2,7 @@ var member = [];
 var total = config.total;
 var digit = 0;
 var tmpTotal = total;
-while (tmpTotal > 0) {
+while (tmpTotal > 0) {//此处是为了确定展示位数digit, tmpTotal只是用来控制循环的临时变量不会被后面的计算用到
     digit++;
     tmpTotal = parseInt(tmpTotal / 10);
 }
