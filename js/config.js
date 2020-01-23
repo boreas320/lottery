bd0f9a01a7be188c34cf1078339b911c430f1598
xiangshuai <xiangshuai@meituan.com>
2020-01-24 01:11:41 +0800

    添加内定获奖名单

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 var config = {
-    total: 450//总人数
+    total: 450, //总人数
+    defaultMember:[]//内定中奖用户名单, 数组中元素是指用户的name, 索引越小的元素中奖的优先级越高. eg: defaultMember: ['430','020'], 优先级函数为P, 则P('430') > P('020') > P(others)
 }
